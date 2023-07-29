137857dfcfb5383ac83a554dbe2f32c892e50e05
Vaishnavi Sarmalkar <99071024+Vaishnavi0805@users.noreply.github.com>
2023-07-29 15:12:11 +0530

    rasa integration

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,86 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+import os
+from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import openai
+from langchain.embeddings.openai import OpenAIEmbeddings
+import pinecone
+from langchain.vectorstores import Pinecone
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Dict, List, Text
+os.environ['OPENAI_API_KEY'] = "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"
 
 
-# This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionQueryDatabase(Action):
+    def name(self) -> str:
+        return "action_query_database"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_query = tracker.latest_message.get('text')
+        os.environ['OPENAI_API_KEY'] = "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"
+        directory = "content"
+        def load_docs(directory):
+            loader = DirectoryLoader(directory)
+            documents = loader.load()
+            return documents
+        documents=load_docs(directory)
+        def split_docs(documents,chunk_size=1000,chunk_overlap=20):
+            text_splitter=RecursiveCharacterTextSplitter(chunk_size=chunk_size,chunk_overlap=chunk_overlap)
+            docs=text_splitter.split_documents(documents)
+            return docs
+        docs = split_docs(documents)
+        embeddings = OpenAIEmbeddings(model_name="text-embedding-ada-002")
+        pinecone.init(
+        api_key="dda3cf0f-f9c6-4245-9ab7-5aa8917e43e2",
+        environment="asia-southeast1-gcp-free")
+
+        index_name="document-querying"
+        index= Pinecone.from_existing_index(index_name, embeddings)
+        def get_similiar_docs(query, k=2, score=False):
+            if score:
+                similar_docs = index.similarity_search_with_score(query, k=k)
+            else:
+                similar_docs = index.similarity_search(query, k=k)
+            return similar_docs
+        model_name = "gpt-3.5-turbo"
+        llm= OpenAI(model_name=model_name)
+        chain = load_qa_chain(llm, chain_type= "stuff")
+
+        def get_answer(query):
+            similar_docs=get_similiar_docs(query)
+            print(similar_docs)
+            print('============================================================================')
+            answer = chain.run(input_documents=similar_docs, question = query)
+            return answer
+    
+       
+        
+        response = get_answer(user_query)
+        # response = agent_executor.run(user_query)
+
+        # # Assuming the response from the database query is a single string
+        dispatcher.utter_message(text=response)
+
+        return []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
